7ffe7ee20d79307c9f59a0ad3cb2982847deb64a
shashank-priyadarshi <shashank9163882019@gmail.com>
2024-07-23 14:15:25 +0530

    refactor(packages): add colored logging and file & file path in logs
    Signed-off-by: Shashank Priyadarshi <shashank9163882019@gmail.com>

--- app/pkg/logger/logger.py
+++ app/pkg/logger/logger.py
@@ -5,6 +5,7 @@ import sys
 from threading import Lock
 
 import yaml
+import coloredlogs
 
 
 class LoggerSingleton:
@@ -18,7 +19,7 @@ class LoggerSingleton:
                     cls._instance = super().__new__(cls)
                     config_path = os.getenv('LOGGING_CONFIG_PATH', 'default_logging.yaml')
                     cls._initialize_logging(config_path)
-                    cls.logger = logging.getLogger('sampleLogger')
+                    cls.logger = logging.getLogger(os.getenv('LOGGER_NAME', 'application'))
         return cls._instance
 
     @classmethod
@@ -27,8 +28,20 @@ class LoggerSingleton:
             with open(config_path, 'r') as f:
                 config = yaml.safe_load(f.read())
                 logging.config.dictConfig(config)
+
+                if 'coloredlogs' in config:
+                    coloredlogs_config = config['coloredlogs']
+                    coloredlogs.install(
+                        level=coloredlogs_config.get('level', 'DEBUG'),
+                        fmt=coloredlogs_config.get('fmt',
+                                                   '[%(pathname)s:%(lineno)d] %(asctime)s %(name)-15s %(levelname)-8s '
+                                                   '%(message)s'),
+                        datefmt=coloredlogs_config.get('datefmt', '%Y-%m-%d %H:%M:%S'),
+                        level_styles=coloredlogs_config.get('level_styles', {}),
+                        field_styles=coloredlogs_config.get('field_styles', {})
+                    )
         except Exception as e:
-            print(f"Failed to load configuration file: {e}", file=sys.stderr)
+            print(f"Failed to load logger configuration file: {e}", file=sys.stderr)
             sys.exit(1)
 
     def get_logger(self):
